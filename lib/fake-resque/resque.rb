96149851fb2d81c9351092034c4c246390542026
Eric Lindvall <eric@5stops.com>
2010-12-30 16:24:56 -0800

    Making things more resque-like.

--- lib/fake-resque/resque.rb
+++ lib/fake-resque/resque.rb
@@ -1,25 +1,30 @@
 module FakeResque
-  class Resque
-    class << self
+  module Resque
+    extend self
+
+    @forward = true
+
+    def enqueue(klass, *args)
+      if @forward
+        klass.perform(*args)
+      end
+    end
+
+    def block!
+      @forward = false
+    end
+
+    def unblock!
       @forward = true
-      def enqueue(klass, *args)
-        if @forward
-          klass.perform(*args)
-        end
-      end
+    end
 
-      def block!
-        @forward = false
-      end
+    def redis=(redis)
+      RealResque.redis = redis
+    end
 
-      def unblock!
-        @forward = true
-      end
-
-      def redis=(redis)
-        RealResque.redis = redis
-      end
-
+    # Mocking some of the Resque methods
+    def queues
+      []
     end
   end
 end
